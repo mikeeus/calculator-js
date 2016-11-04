a7f2474b83981433139955d40d084ecc31d48c8c
mikeeus <itsmikias@gmail.com>
2016-11-04 18:18:26 +0300

    Handle edge cases

--- public/javascripts/calculator.js
+++ public/javascripts/calculator.js
@@ -8,8 +8,7 @@ function handleClick(element){
   content = element.innerHTML;
   switch(content){
     case "CE":
-      current = "0";
-      calculation = "";
+      clearDisplay();
       break;    
     case "1":
     case "2":
@@ -20,96 +19,133 @@ function handleClick(element){
     case "7":
     case "8":
     case "9":
-      if(totaled == true){
-        calculation = "";
-        calculation = content;
-        current = content;
-        totaled = false;
-        break;
-      }
-      if(current.length > 14){ current = current.slice(0,15); break;}    
-      if(current == "0" || ["×", "÷", "+", "-"].includes(current)){
-        calculation = calculation.concat(content);        
-        current = content;
-        break;
-      }
-      calculation = calculation.concat(content);
-      current = current.concat(content);   
+      handleNumber();
       break;
     case "0":
     case "00":
-      if(totaled == true){
-        calculation = "";
-        current = content;
-        totaled = false;
-        break;
-      }
-      if(current.length > 14){ current = current.slice(0,15); break;}        
-      if(current == "0" || current == "00"){
-        console.log('not adding to zero');
-        break;
-      }
-      calculation = calculation.concat(content);
-      current = current.concat(content);
+      handleZero();
       break;
     case ".":
-      if(totaled == true){
-        calculation = "";
-        calculation = "0" + content;        
-        current = "0" + content;
-        totaled = false;
-        break;
-      }
-      if(current.length > 14){ current = current.slice(0,15); break;}    
-      if(!current.split("").includes(".")){
-        current = current.concat(content);
-        calculation = calculation.concat(content);        
-        break;
-      } else {
-        console.log('multiple "."s');
-        break;
-      }    
+      handleDecimal();
+      break;
     case "×":
     case "÷":
     case "+":
     case "-":
-      if(totaled == true){
-        totaled = false;
-      }
-      if(calculation == "" && current == 0){
-        console.log("empty");
-        break;
-      }
-      if(["×", "÷", "+", "-"].includes(calculation.trim().slice(-1))){
-        console.log("cannot double operations");
-        break;
-      }
-      calculation = calculation.concat(" " + content + " ");
-      current = content;     
+      handleOperator();
       break;
     case "=":
-      console.log('eval calculation, set current to 0, set calc to sum');
-      calcString = calculation.replace(/×/g, "*").replace(/÷/g,"/");
-      calculation = eval(calcString);
-      if(calculation == 'Infinity'){
-        current = "Invalid Operation"
-        break;
-      }
-      if(calculation > 999999999999999 || calculation.length > 15){
-        current = "Error: too large";
-        break;
-      }
-      totaled = true;
-      calculation = calculation.toString().slice(0,16);
-      current = calculation;
+      handleEquals();
       break;
   }
-  console.log('calc: ', calculation);
-  console.log('current: ', current);  
   updateDisplay(current, calculation);
 }
 
 function updateDisplay(curr, calc){
   $("#current").html(curr)
   $("#calculation").html(calc)  
+}
+
+function handleNumber(){
+  if(totaled == true){
+    calculation = "";
+    calculation = content;
+    current = content;
+    totaled = false;
+    return;
+  }
+  if(current.length > 14){ current = current.slice(0,15); return;}    
+  if(current == "0" || ["×", "÷", "+", "-"].includes(current)){
+    calculation = calculation.concat(content);        
+    current = content;
+    return;
+  }
+  calculation = calculation.concat(content);
+  current = current.concat(content);   
+  return;
+}
+
+function handleOperator(){
+  if(totaled == true){
+    totaled = false;
+  }
+  if(calculation == "" && current == 0){
+    console.log("Cannot add operation to empty calculation.");
+    return;
+  }
+  if(["×", "÷", "+", "-"].includes(calculation.trim().slice(-1))){
+    console.log("Cannot chain operations");
+    return;
+  }
+  calculation = calculation.concat(" " + content + " ");
+  current = content;
+  if(calculation.length > 60){
+    console.log('too long');
+    current = "Too many digits."
+    setTimeout(function(){ clearDisplay(); }, 1000);
+    return;
+  }
+}
+
+function handleZero(){
+  if(totaled == true){
+    calculation = "";
+    current = content;
+    totaled = false;
+    return;
+  }
+  if(current.length > 14){ current = current.slice(0,15); return;}        
+  if(current == "0" || current == "00"){
+    console.log('Cannot add to zero.');
+    return;
+  }
+  calculation = calculation.concat(content);
+  current = current.concat(content);
+  return;
+}
+
+function handleDecimal(){
+  if(totaled == true){
+    calculation = "";
+    calculation = "0" + content;        
+    current = "0" + content;
+    totaled = false;
+    return;
+  }
+  if(current == "0"){
+    current = "0.";
+    calculation = calculation.concat(current);
+    return;
+  }
+  if(current.length > 14){ current = current.slice(0,15); return;}    
+  if(!current.split("").includes(".")){
+    current = current.concat(content);
+    calculation = calculation.concat(content);        
+    return;
+  } else {
+    console.log('Cannot contain multiple decimal points.');
+    return;
+  }
+}
+
+function handleEquals(){
+  calcString = calculation.replace(/×/g, "*").replace(/÷/g,"/");
+  calculation = eval(calcString);
+  if(calculation == 'Infinity'){
+    current = "Invalid Operation"
+    return;
+  }
+  if(calculation > 999999999999999){
+    current = "Error: too large";
+    return;
+  }
+  totaled = true;
+  calculation = parseFloat(calculation).toString().slice(0,16);
+  current = calculation;
+  return;
+}
+function clearDisplay(){
+  current = "0";
+  calculation = "";
+  updateDisplay(current, calculation);
 }
